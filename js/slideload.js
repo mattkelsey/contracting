8388b5a8e00f81fb7ba93195bbeee2e5a0fa5d7a
mattkelsey <mattkelseymk@gmail.com>
2016-04-05 14:25:21 -0700

    Add todo and galleryjs

---
+++ js/slideload.js
@@ -0,0 +1,20 @@
+function slideLoad() {
+    var dir = "gallery/img/slideshow";
+    var fileextension = ".jpg";
+    var nameArray = [];
+    $.ajax({
+        url: dir,
+        sucess: function (data) {
+            var location = 0;
+            $(data).find("a:contains(" + fileextension + ")").each(
+                function() {
+                    nameArray[location] = this.href.replace(window.location.host, "").replace ("http:///", "").replace("test/", "");
+                    location++;
+
+                }
+            );
+            console.log(nameArray);
+            return nameArray;
+        }
+    });
+}
